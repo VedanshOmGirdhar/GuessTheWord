696ac4a6e4873fa8cce0f476415afc49d825ea2a
VedanshOmGirdhar <80617090+VedanshOmGirdhar@users.noreply.github.com>
2021-08-29 13:01:15 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+function addUser(){
+    player1_name = document.getElementById("p1_name_input").value;
+    player2_name = document.getElementById("p2_name_input").value;
+
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+
+    window.location.replace("game_page.html");
+    
+}
